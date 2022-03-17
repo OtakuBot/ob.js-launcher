7bf7426d388117a5e93ca83d4c9be5d6cbcd9aaa
NightcoreAT <oussamakatalonya1@gmail.com>
2022-03-17 14:50:38 +0100

    Steps #1-2-3

---
+++ main.js
@@ -0,0 +1,39 @@
+//-----------------------------------------------------------------//
+//-------------The main file required to run the bot---------------//
+//------------------Made By NightcoreATDZO#3550--------------------//
+//---------------------------OtakuBot------------------------------//
+//-----------------------------------------------------------------//
+
+const {Collection, Client, Discord} = require('discord.js')
+const config = require('./config.json');
+const fs = require('fs');
+const client = new Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "GUILD_PRESENCES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+    ],
+});
+
+module.exports = client;
+
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+["command", "event"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.login(config.token)
